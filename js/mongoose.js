f516e3a31d112dff415c8db4c6f24608e98d7d57
Evan Bush <e.bush05@gmail.com>
2021-07-09 19:52:00 -0400

    removes practice module files

--- js/mongoose.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://<demo-user1>:<URD2pRBrerc2avCt>@cluster0.tnvmw.mongodb.net/ClassRoomDB?retryWrites=true&w=majority', {
-    useMongoClient: true
-})
