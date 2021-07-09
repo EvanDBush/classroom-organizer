efcb666a5ea0934ee1466837ea7f643da72e25b5
Evan Bush <e.bush05@gmail.com>
2021-07-09 13:12:09 -0400

    attempting to connect to mongDB

---
+++ js/mongoose.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://<demo-user1>:<URD2pRBrerc2avCt>@cluster0.tnvmw.mongodb.net/ClassRoomDB?retryWrites=true&w=majority', {
+    useMongoClient: true
+})
