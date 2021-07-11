d78b62c4114fe62369e3de6ea867ea89b1b88115
Evan Bush <e.bush05@gmail.com>
2021-07-10 21:41:39 -0400

    adds console log to server.listen

--- server.js
+++ server.js
@@ -7,4 +7,6 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
 //Start server
-server.listen(port);
+server.listen(port, () => {
+    console.log(`the server is running on port: ${port}`)
+});
