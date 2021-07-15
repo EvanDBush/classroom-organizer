8b847a321b70ae3d95c1c0fc4cc184c4321bd8a7
Evan Bush <e.bush05@gmail.com>
2021-07-15 15:30:18 -0400

    removes password from code

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const messageRoutes = require('./api/routes/messages');
 //     '@cluster0.tnvmw.mongodb.net/classroomDB?retryWrites=true&w=majority',
 //     { useUnifiedTopology: true, useNewUrlParser: true })
 
-mongoose.connect('mongodb+srv://demo-user1:URD2pRBrerc2avCt@cluster0.tnvmw.mongodb.net/ClassRoomDB?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://demo-user1:' +process.env.MONGO_ATLAS_PW+ '@cluster0.tnvmw.mongodb.net/ClassRoomDB?retryWrites=true&w=majority',
 { useUnifiedTopology: true, useNewUrlParser: true });
 
 //  middleware
