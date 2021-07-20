f5f0896f56d2f3ad76b9f4a55df2118428193b1f
Evan Bush <e.bush05@gmail.com>
2021-07-20 13:38:39 -0400

    displays classroom array on index.html

--- js/classrooms.js
+++ js/classrooms.js
@@ -18,3 +18,12 @@ const figs = new Classroom('figs', [6/30/2021, 1/1/2021], []);
 const grapes = new Classroom('grapes', [12/31/2020, 7/1/2020], []);
 const lemons = new Classroom('lemons', [6/30/2020, 1/1/2020], []);
 const apples = new Classroom('apples', [12/31/2019, 7/1/2019], []);
+
+let classroomsArray = [figs, grapes, lemons, apples]
+const classroomList = document.getElementById('classroom-ulist')
+
+classroomsArray.forEach(classroom => {
+    let listItem = document.createElement('li')
+    listItem.textContent = `${classroom.roomName} ${classroom.students.length}`
+    classroomList.appendChild(listItem)
+})
