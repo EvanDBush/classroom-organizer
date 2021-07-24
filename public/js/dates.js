5ec390021a9296f661274e40d44b1d38745509ef
Evan Bush <e.bush05@gmail.com>
2021-07-24 16:37:44 -0400

    adds description paragraph when date is clicked

--- public/js/dates.js
+++ public/js/dates.js
@@ -10,13 +10,26 @@ document.addEventListener('DOMContentLoaded', function() {
     })
     });
 
+const dateList = document.getElementById('date-list');
+
 function buildDateList(dates) {
     dates.forEach(date => {
         let item = document.createElement('li')
         item.textContent = `${date.name} ${date.date[0]}`
-        document.getElementById('date-list').appendChild(item)        
+        dateList.appendChild(item)        
     });
 }
+
+dateList.addEventListener('click', (event) => {
+const dateNameClicked = event.target.textContent.split(" ");
+dateData.forEach(date => {
+    if (date.name.split(" ")[0] === dateNameClicked[0]) {
+        let descriptionP = document.createElement('p');
+        descriptionP.textContent = `${date.name}: ${date.description}`;
+        document.getElementById('test').appendChild(descriptionP);
+    }
+})
+})
 
 
 // Displays current date
