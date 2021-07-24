018ab427584688a0ece38701467bad91ab6141fd
Evan Bush <e.bush05@gmail.com>
2021-07-24 13:31:39 -0400

    trying to access classroom.ageDates array. still undefined

--- public/js/classrooms.js
+++ public/js/classrooms.js
@@ -1,18 +1,21 @@
 class Classroom {
     constructor( roomName, ageDates, students) {
     this.roomName = roomName;
-    this.ageDates = [new Date(ageDates[0]), new Date(ageDates[1])];
+    this.ageDates = ageDates;
     this.students = students;
     }
 }
 
-const schoolYearStartDate = new Date(08/01/2021);
+const schoolYearStartDate = new Date('08/01/2021');
 const todaysDate = new Date();
 
 const figs = new Classroom('figs', ["06/30/2021", "01/01/2021"], []);
 const grapes = new Classroom('grapes', ["12/31/2020", "07/01/2020"], []);
 const lemons = new Classroom('lemons', ["06/30/2020", "01/01/2020"], []);
 const apples = new Classroom('apples', ["12/31/2019", "07/01/2019"], []);
+
+console.log(new Date(figs.ageDates[0]))
+
 
 let classroomsArray = [figs, grapes, lemons, apples]
 const classroomList = document.getElementById('classroom-ulist')
@@ -28,13 +31,14 @@ classroomList.addEventListener('click', (event) => {
     organizeByAge(studentData, roomClicked);
 });
 
-function organizeByAge(students, roomClicked) {
+function organizeByAge(studentData, roomClicked) {
     const classroom = classroomsArray.find((room) => room.roomName === roomClicked);
-    students.forEach((student) => {
+    studentData.forEach((student) => {
         const bDay = new Date(student.birthDate);
-        if ((classroom.ageDates[1] >= bDay) && (bDay >= classroom.ageDates[0])) {
-            classroom.students.push(student);
+        if ((bDay <= new Date(classroom.ageDates[1])) && (bDay >= new Date(classroom.ageDates[0]))) {
+            classroom.students.push(student);   
         }        
     });
+    console.log(classroom.students);
 }
 
