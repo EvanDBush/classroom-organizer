0eebb984c1bf8e212dfef6a91c498d876cf4823b
Evan Bush <e.bush05@gmail.com>
2021-07-24 21:58:43 -0400

    changes commas to / in todays date display

--- public/js/dates.js
+++ public/js/dates.js
@@ -1,5 +1,13 @@
 let dateData = null;
 
+// Displays current date
+const timeDisplay = document.getElementById('time-display');
+const today = new Date();
+const [month, day, year] = 
+    [today.getMonth(), today.getDate(), today.getFullYear()];
+
+timeDisplay.textContent = `Today's Date: ${month}/${day}/${year}`;
+
 document.addEventListener('DOMContentLoaded', function() {
     fetch('/dates')
     .then(result => result.json())
@@ -14,6 +22,10 @@ const dateList = document.getElementById('date-list');
 
 function buildDateList(dates) {
     dates.forEach(date => {
+        date.date.forEach(day => new Date(day));
+    })
+    const sortedDateData = dates.slice().sort((a,b) => b.date[0] - a.date[0])
+    sortedDateData.forEach(date => {
         let item = document.createElement('li')
         item.textContent = `${date.name} ${date.date[0]}`
         dateList.appendChild(item)        
@@ -26,16 +38,15 @@ dateData.forEach(date => {
     if (date.name.split(" ")[0] === dateNameClicked[0]) {
         let descriptionP = document.createElement('p');
         descriptionP.textContent = `${date.name}: ${date.description}`;
+        
+        const daysTill = Math.floor(((new Date(date.date[0]).getTime()) - today.getTime())/(1000 * 60 * 60 * 24))
+        let counterP = document.createElement('p');
+        counterP.textContent = `${daysTill} days until ${date.name} !!!`
+
         document.getElementById('test').appendChild(descriptionP);
+        document.getElementById('test').appendChild(counterP);
     }
 })
 })
 
 
-// Displays current date
-const timeDisplay = document.getElementById('time-display');
-const today = new Date();
-const [month, day, year] = 
-    [today.getMonth(), today.getDate(), today.getFullYear()];
-
-timeDisplay.textContent = `Today's Date: ${[month, day, year]}`;
